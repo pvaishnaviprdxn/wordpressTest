2a1edf27ed7593ed1313d5939e5647e7812c1252
Vaishnavi P <vaishnavip@prdxn.com>
2020-08-13 12:08:09 +0530

    header and footer

---
+++ wp-content/themes/mytheme/footer.php
@@ -0,0 +1,9 @@
+      </main>
+      <footer>
+        <div class="wrapper">
+          <span><?php bloginfo('name');?> &copy; <?php echo date('Y'); ?></span>
+        </div>
+      </footer>
+    </div>
+  </body>
+</html>
---
+++ wp-content/themes/mytheme/header.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <title><?php bloginfo('name'); ?></title>
+  <meta charset="<?php bloginfo('charset'); ?>">
+  <meta name="viewport" content="width=device-width">
+  <?php wp_head(); ?>
+</head>
+<body>
+  <div class="container">
+    <header>
+      <div class="wrapper">
+        <h1><a href="<?php home_url(); ?>"></a></h1>
+      </div>
+    </header>
+    <main>
