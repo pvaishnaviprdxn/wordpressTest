dd23dad683f163b39351848b34f7ea541e7ab157
Vaishnavi P <vaishnavip@prdxn.com>
2020-08-13 18:56:03 +0530

    random posts functionality done

--- wp-content/themes/mytheme/template-parts/content-relatedposts.php
+++ wp-content/themes/mytheme/template-parts/content-relatedposts.php
@@ -1,20 +1,27 @@
 <?php 
 $featured_posts = get_field('related_posts');
 $title = get_field('title');
-if( $featured_posts && $title ) { ?>
+if( $featured_posts || $title ) { ?>
   <section class="realted-posts">
     <div class="wrapper">
-      <h2><?php echo $title; ?></h2>
+      <?php
+        if ($title) {
+          echo $title ? '<h2>'.$title.'</h2>' : null;
+        } else {
+          echo "<h2>Related Posts</h2>";
+        }
+      ?>
       <ul>
         <?php foreach( $featured_posts as $featured_post ) {
             $permalink = get_permalink( $featured_post->ID );
             $title = get_the_title( $featured_post->ID );
-            $custom_field = get_field( 'content', $featured_post->ID );
-            $excerpt = get_the_excerpt($featured_post->ID);
-            ?>
-            <li>
-                <h3><a href="<?php echo esc_url( $permalink ); ?>"><?php echo esc_html( $title ); ?></a></h3>
-                <?php echo $excerpt ? '<p>'.$excerpt.'</p>' : null; ?>
+            $featuredImage = get_the_post_thumbnail($featured_post->ID);
+          ?>
+            <li>              
+              <?php echo $featuredImage ? '<figure class="featured-image">'.$featuredImage.'</figure>' : null; ?>
+              <?php if($title) { ?> 
+                <h3><a href="<?php echo $permalink ?>"><?php echo $title; ?></a></h3> 
+              <?php } ?>
             </li>
         <?php } ?>
       </ul>
